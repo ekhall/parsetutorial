bd0f3ff506f29ea62a37739043f313d7a9626a67
E. Kevin Hall <hall@absinthe.org>
2012-12-05 17:37:09 -0500

    Wall code added

--- TutorialBase/WallPicturesViewController.m
+++ TutorialBase/WallPicturesViewController.m
@@ -88,7 +88,45 @@
 //Load the images on the wall
 -(void)loadWallViews
 {
-    //TODO: Put the wall objects in the scroll view
+    // Clean the scroll view
+    for (id viewToRemove in [self.wallScroll subviews]) {
+        if ([viewToRemove isMemberOfClass:[UIView class]])
+            [viewToRemove removeFromSuperview];
+    }
+    
+    // For every wall element, put a view in the scroll
+    int originY = 10;
+    for (PFObject *wallObject in self.wallObjectsArray) {
+        // Build the view with the image and comments
+        UIView *wallImageView = [[UIView alloc] initWithFrame:
+                                 CGRectMake(10, originY, self.view.frame.size.width -20, 300)];
+        
+        // Add the image
+        PFFile *image = (PFFile *)[wallObject objectForKey:@"image"];
+        UIImageView *userImage = [[UIImageView alloc] initWithImage:[UIImage imageWithData:image.getData]];
+        userImage.frame = CGRectMake(0, 0, wallImageView.frame.size.width, 200);
+        [wallImageView addSubview:userImage];
+        
+        // Add the info label (User and Creation date)
+        NSDate *creationDate = wallObject.createdAt;
+        NSDateFormatter *df = [[NSDateFormatter alloc] init];
+        [df setDateFormat:@"HH:mm dd/MM yyyy"];
+        
+        UILabel *infoLabel = [[UILabel alloc] initWithFrame:
+                              CGRectMake(0, 210, wallImageView.frame.size.width, 15)];
+        infoLabel.text = [NSString stringWithFormat:@"Uploaded by: %@, %@",
+                          [wallObject objectForKey:@"user"],
+                          [df stringFromDate:creationDate]];
+        infoLabel.font = [UIFont fontWithName:@"Arial-ItalicMT" size:9];
+        infoLabel.textColor = [UIColor whiteColor];
+        infoLabel.backgroundColor = [UIColor clearColor];
+        [wallImageView addSubview:infoLabel];
+        
+        // Add the comment
+        UILabel *commentLabel = [[UILabel alloc] initWithFrame:
+                                 CGRectMake(0, 240, wallImageView.frame.size.width, 15)];
+        
+    }
     
 }
 
